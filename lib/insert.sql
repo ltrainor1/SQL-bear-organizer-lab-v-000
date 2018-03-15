3d5b30fb78f1ddd2cecb34a3ece4f2964da479aa
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-15 18:03:07 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES 
+(1,'Mr. Chocolate', 3, 'M', 'chocolatey brown', 'nice', 1),
+(2,'Rowdy', 3, 'M', 'black', 'fun', 1),
+(3,'Tabitha', 3, 'F', 'light brown', 'passive aggressive', 1),
+(4,'Sergeant Brown', 3, 'M', 'brown', 'dominant', 1),
+(5,'Melissa', 3, 'F', 'white', 'helpful', 1),
+(6,'Grinch', 3, 'F', 'green', 'christmasey', 1),
+(7,'Wendy', 3, 'F', 'gray', 'capricious', 1),
+(8, null, 3, 'M', 'striped', 'evil', 1);
